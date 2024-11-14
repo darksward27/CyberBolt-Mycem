7fba3d51fa91804a49c6943c94eee1004bad6c4c
darksward27 <joelapeter82@gmail.com>
2024-11-14 18:12:14 +0530

    added Genesis block

--- Node.py
+++ Node.py
@@ -1,5 +1,6 @@
 import threading
 import hashlib
+import time
 
 class Blockchain:
     def __init__(self):
@@ -11,3 +12,15 @@ class Blockchain:
         self.projects = {}
         self.wallets = {}
         self.token_counter = 0
+
+
+    def create_genesis_block(self):
+            genesis_block = {
+                'index': 1,
+                'timestamp': time(),
+                'transactions': [],
+                'proof': 100,
+                'previous_hash': '1',
+            }
+            self.chain.append(genesis_block)
+            print("Genesis block created")
